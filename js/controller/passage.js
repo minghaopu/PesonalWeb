c67304041f39d552ce646cbcefe154515d430416
roneilPMH <roneil_PMH@hotmail.com>
2016-06-23 18:03:11 -0400

    Modify passage module;

--- js/controller/passage.js
+++ js/controller/passage.js
@@ -1,4 +1,49 @@
-mpw.controller("passage", ["$scope", "$routeParams", function($scope, $routeParams){
-	$scope.someText = "passage";
+mpw.controller("passage", ["$scope", "$routeParams", "$request", "$location", function($scope, $routeParams, $request, $location) {
 	$scope.passageId = $routeParams.passageId;
+	$scope.widget = {
+		dynamic: {
+			data: ""
+		},
+		edit: {
+			text: "Edit",
+			fn: function() {
+				$location.path("/edit/" + $scope.passageId);
+			}
+		},
+		delete: {
+			text: "Delete",
+			fn: function() {
+				$request.query({
+					url: "./data/passage.json",
+					data: {
+						action: "deletePassage",
+						data: {
+							passageId: $scope.passageId
+						}
+					}
+				}, function(data) {
+					$scope.widget.dynamic.data = data.content
+					$scope.title = data.title;
+					$scope.time = data.createtime;
+				}, function() {
+
+				})
+			}
+		}
+	}
+	$request.query({
+		url: "./data/passage.json",
+		data: {
+			action: "getPassage",
+			data: {
+				passageId: $scope.passageId
+			}
+		}
+	}, function(data) {
+		$scope.widget.dynamic.data = data.content
+		$scope.title = data.title;
+		$scope.time = data.createtime;
+	}, function() {
+
+	})
 }])
