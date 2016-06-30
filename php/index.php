f17f2f6ceb9072d2002f0b828a52f10aff8ea673
roneilPMH <roneil_PMH@hotmail.com>
2016-06-30 14:42:11 -0400

    no message

--- php/index.php
+++ php/index.php
@@ -1,64 +1,65 @@
 
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
-	ini_set('session.user_only_cookies', true);
-	ini_set('session.cookie_httponly', 1);
-	ini_set('session.name', '_KLJ');
-	ini_set('session.cookie_lifetime', 84600);
-	ini_set('session.gc_maxlifetime', 84600);
+	error_reporting(E_ALL);
+	ini_set('display_errors',1);
+	phpinfo();
+	// ini_set('session.user_only_cookies', true);
+	// ini_set('session.cookie_httponly', 1);
+	// ini_set('session.name', '_KLJ');
+	// ini_set('session.cookie_lifetime', 84600);
+	// ini_set('session.gc_maxlifetime', 84600);
 
-	require_once 'util.php';
-	require_once 'db.php';
-	require_once 'json.php';
-	require_once 'user.php';
-	require_once 'blog.php';
-	require_once 'file.php';
-
-
-	date_default_timezone_set("UTC");
-
-	$GLOBALS['islogged'] = false;
-	$url = $_SERVER['REQUEST_URI'];
+	// require_once 'util.php';
+	// require_once 'db.php';
+	// require_once 'json.php';
+	// require_once 'user.php';
+	// require_once 'blog.php';
+	// require_once 'file.php';
 
 
-	if (preg_match('/php/i', $url)) {
-		// echo '1';
-		$user = new User;
-		$blog = new Blog;
-		$file = new File;
-		// echo '2';
-		// echo $user;
-		// $passage = new Passage;
-		$module = preg_split('/\/php\//', $url)[1];
+	// date_default_timezone_set("UTC");
+
+	// $GLOBALS['islogged'] = false;
+	// $url = $_SERVER['REQUEST_URI'];
 
 
-		switch ($module) {
-			case 'user':
-				$param = json_decode(file_get_contents('php://input'));
-				$methodName = $param->action;
-				$data = $param->data;
-				$user->{$methodName}($data);
-				break;
-			case 'passage':
-				$param = json_decode(file_get_contents('php://input'));
-				$methodName = $param->action;
-				$data = $param->data;
-				$blog->{$methodName}($data);
-				break;
-			case 'resume':
-				$file->upload('resume');
-				break;
-			case 'image':
-				$file->upload('image');
-				break;
-			default:
-				header('Location: /');
-		}
+	// if (preg_match('/php/i', $url)) {
+	// 	// echo '1';
+	// 	$user = new User;
+	// 	$blog = new Blog;
+	// 	$file = new File;
+	// 	// echo '2';
+	// 	// echo $user;
+	// 	// $passage = new Passage;
+	// 	$module = preg_split('/\/php\//', $url)[1];
 
-	}else {
-		header('Location: /');
-	}
+
+	// 	switch ($module) {
+	// 		case 'user':
+	// 			$param = json_decode(file_get_contents('php://input'));
+	// 			$methodName = $param->action;
+	// 			$data = $param->data;
+	// 			$user->{$methodName}($data);
+	// 			break;
+	// 		case 'passage':
+	// 			$param = json_decode(file_get_contents('php://input'));
+	// 			$methodName = $param->action;
+	// 			$data = $param->data;
+	// 			$blog->{$methodName}($data);
+	// 			break;
+	// 		case 'resume':
+	// 			$file->upload('resume');
+	// 			break;
+	// 		case 'image':
+	// 			$file->upload('image');
+	// 			break;
+	// 		default:
+	// 			header('Location: /');
+	// 	}
+
+	// }else {
+	// 	header('Location: /');
+	// }
 
 
 
