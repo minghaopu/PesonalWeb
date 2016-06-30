9001253c37cac68a85854591e9cb50c4d671b4bd
roneilPMH <roneil_PMH@hotmail.com>
2016-06-30 01:22:49 -0400

    php init

--- php/index.php
+++ php/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 	ini_set('session.user_only_cookies', true);
 	ini_set('session.cookie_httponly', 1);
 	ini_set('session.name', '_KLJ');
